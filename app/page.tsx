ed56b51492472fcba54ecb2cf7acdfb6a475312e
Umang <umangchaudhary2674@gmail.com>
2025-11-23 19:48:32 +0530

    Initial setup check

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
+/**
+ * Default Home Page of the application.
+ * @returns 
+ */
+
 export default function Home() {
   return (
    <div>
